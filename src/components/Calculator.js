53f50b829d72c1a0ea033295ccc7281dd95c469d
NathanYee412 <nyee@csu.fullerton.edu>
2022-04-17 21:16:39 -0700

    css padding added

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -27,7 +27,6 @@ function Calculator(props) {
         total = Number(total) + Number(billAmount);
 
         setTotalPerPerson(total);
-
     }
 
     function handleCustomTip(tip) {
@@ -50,7 +49,14 @@ function Calculator(props) {
         total = total / Number(people);
 
         setTotalPerPerson(total);
+    }
 
+    function reset() {
+        setBillAmount(0);
+        setPercentTip(1);
+        setNumPeople(1);
+        setTotalPerPerson(0);
+        window.location.reload(false);
     }
 
 
@@ -60,46 +66,51 @@ function Calculator(props) {
                 <h1>S P L I<br />T T E R</h1> 
             </div>
             <div className='options'>
-                <h2>Bill</h2>
-                <form onSubmit={onSubmit}>
-                    <label>
-                        <input type='text' name='billAmount' onChange={event => handleBillAmount(event.target.value) }></input>
-                    </label>
-                </form>
-                <h2>Select Tip %</h2>
-                <form onSubmit={onSubmit} className='container'>
-                    <div className='col'>
-                        <button onClick={() => handleTip(0.05)}>5%</button>
-                        <button onClick={() => handleTip(0.15)}>15%</button>
-                        <button onClick={() => handleTip(0.50)}>50%</button>
-                    </div>
-                    <div className='col'>
-                        <button onClick={() => handleTip(0.1)}>10%</button>
-                        <button onClick={() => handleTip(0.25)}>25%</button>
+                <div className='bill'>
+                    <h2>Bill</h2>
+                    <form onSubmit={onSubmit}>
                         <label>
-                            <input type='text' name='customPercent' onChange={event => handleCustomTip(event.target.value)}></input>
+                            <input type='text' name='billAmount' onChange={event => handleBillAmount(event.target.value) }></input>
                         </label>
+                    </form>
+                </div>
+                <div className='tip'>
+                    <h2>Select Tip %</h2>
+                    <form onSubmit={onSubmit} className='container'>
+                        <div className='col'>
+                            <button onClick={() => handleTip(0.05)}>5%</button>
+                            <button onClick={() => handleTip(0.15)}>15%</button>
+                            <button onClick={() => handleTip(0.50)}>50%</button>
+                        </div>
+                        <div className='col'>
+                            <button onClick={() => handleTip(0.1)}>10%</button>
+                            <button onClick={() => handleTip(0.25)}>25%</button>
+                            <label>
+                                <input type='text' name='customPercent' onChange={event => handleCustomTip(event.target.value)}></input>
+                            </label>
+                        </div>
+                    </form>
+                </div>
+                <div className='people'>
+                    <h2>Number of People</h2>
+                    <form onSubmit={onSubmit}>
+                        <label>
+                            <input type='text' name='numPeople' onChange={event => handleNumPeople(event.target.value)}></input>
+                        </label>
+                    </form>    
+                </div>         
+                <div className='results'>
+                    <div className='container'>
+                        <h3>Tip Amount <br />/ person</h3>
+                        <h3>${billAmount * percentTip / numPeople}</h3>
                     </div>
-                </form>
-                <h2>Number of People</h2>
-                <form onSubmit={onSubmit}>
-                    <label>
-                        <input type='text' name='numPeople' onChange={event => handleNumPeople(event.target.value)}></input>
-                    </label>
-                </form>             
-            </div>
-            <div className='results'>
-                <div className='container'>
-                    <h3>Tip Amount <br />/ person</h3>
-                    <h2>${billAmount * percentTip / numPeople}</h2>
+                    <div className='container'>
+                        <h3>Total <br />/ person</h3>
+                        <h3>${totalPerPerson}</h3>
+                    </div>
+                    <button onClick={() => reset()}>Reset</button>  
                 </div>
-                <div className='container'>
-                    <h3>Total <br />/ person</h3>
-                    <h2>${totalPerPerson}</h2>
-                </div>
-                <button>Reset</button>  
             </div>
-            
         </div>
     );
 }
